7a4489a37e3adf15ab4a2b457ed42293ca47313c
SunBinHong00 <110758221+SunBinHong00@users.noreply.github.com>
2024-04-20 03:50:09 +0900

    [level 2] Title: n^2 배열 자르기, Time: 27.06 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,18 +1,44 @@
-import math
+# import math
 
-def s(n,index):
+# def s(n,index):
     
-    # 층수
-    floor = math.ceil(index/n)
-    # 가로에서 몇번째 
-    idx = index%n
-    # idx==0 가로 끝인 경우 n
-    idx = n if idx==0 else idx
+#     # 층수
+#     floor = math.ceil(index/n)
+#     # 가로에서 몇번째 
+#     idx = index%n
+#     # idx==0 가로 끝인 경우 n
+#     idx = n if idx==0 else idx
     
-    return max(floor, idx)
+#     return max(floor, idx)
 
+# def solution(n, left, right):
+    
+    
+    
+#     return [s(n,idx+1) for idx in range(left, right+1)]
+
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
 def solution(n, left, right):
-    
-    
-    
-    return [s(n,idx+1) for idx in range(left, right+1)]
+    answer = []
+
+    for i in range(left, right+1):
+        q, r = divmod(i, n)
+
+        answer.append(max(q, r) + 1)
+
+
+
+
+    return answer
