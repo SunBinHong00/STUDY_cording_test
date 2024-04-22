359b1f5b99c7c881cac4589b4df5193f96e93bb5
SunBinHong00 <110758221+SunBinHong00@users.noreply.github.com>
2024-04-22 21:26:35 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,22 @@
+import math
+def solution(progresses, speeds):
+    schedule = [math.ceil((100-progress)/speed) for progress, speed in zip(progresses,speeds)]
+
+    tmp = []
+    answer = 0
+
+    result = []
+
+    for i in (schedule):
+        print(i)
+        if not tmp:
+            tmp.append(i)
+
+        elif tmp[0] >= i:
+            tmp.append(i)
+        else:
+            result.append(len(tmp))
+            tmp=[]
+            tmp.append(i)
+    result.append(len(tmp))
+    return result
