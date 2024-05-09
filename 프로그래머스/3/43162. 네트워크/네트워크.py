8983bee9be489829779d876192fc7b97be45a51c
SunBinHong00 <110758221+SunBinHong00@users.noreply.github.com>
2024-05-09 19:45:06 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -6,32 +6,49 @@ def solution(n, computers):
     def dfs(stack):
 
         while stack:
-            try:
-                idx = stack.pop()
-                print(idx)
-                history.append(idx)
 
-                nums = computers[idx]
+            idx = stack.pop()
+            history.append(idx)
+            nums = computers[idx]
+            will_go = [i for i,v in enumerate(nums) if v==1 and i not in history]
 
-                will_go = [i for i,v in enumerate(nums) if v==1 and i not in history]
+            if will_go:
+                stack.extend(will_go)
+                
+            else:
+                
+                if stack:
+                    continue
 
-                if will_go:
-                    stack.extend(will_go)
                 else:
+                    nonlocal answer
+                    answer += 1
+                    for i in range(n):
+                        if i not in history:
+                            dfs([i])
+                            break     
 
-                    if stack:
-                        continue
-
-                    else:
-                        nonlocal answer
-                        answer += 1
-                        for i in range(n+1):
-                            if i not in history:
-                                dfs([i])
-                                break     
-            except:
-                pass
     dfs([0])
     return answer
+def solution(n, computers):
+    visited = set()
+    answer = 0
+
+    def dfs(node):
+        stack = [node]
+        while stack:
+            current = stack.pop()
+            if current not in visited:
+                visited.add(current)
+                # Enqueue all connected, unvisited nodes
+                stack.extend([i for i, connected in enumerate(computers[current]) if connected == 1 and i not in visited])
+
+    for i in range(n):
+        if i not in visited:
+            dfs(i)
+            answer += 1  # Each successful DFS from a new start node indicates a new network
+
+    return answer
+
 
     
